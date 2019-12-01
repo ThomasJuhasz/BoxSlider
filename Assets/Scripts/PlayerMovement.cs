a9cc7a5a304bb29b9271af578b5e97c75ffd503c
ThomasJuhasz <juhasz.thomas.jozsef@gmail.com>
2019-12-01 20:13:15 +0100

    Level 1 and Level 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rigidBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rigidBody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rigidBody.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
