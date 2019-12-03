a2662544eca981e97a6991b7888119ca36a60c40
ThomasJuhasz <juhasz.thomas.jozsef@gmail.com>
2019-12-03 17:05:27 +0100

    joystick pack, but code doesnt use it
    - implemented tap controlling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+enum MoveDirection { Right, Left }
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rigidBody;
-    public float forwardForce = 500f;
-    public float sidewaysForce = 500f;
+    public float forwardForce = 1f;
+    public float sidewaysForce = 50f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    float timeBetweenTaps = .01f;
+    float tapTimer = 0;
 
+    int _lane = 3;
+    int lane {
+        get {
+            return _lane;
+        }
+        set {
+            value = value > 5 ? 5 : value;
+            value = value < 1 ? 1 : value;
+            _lane = value;
+        }
     }
 
-    // Update is called once per frame
+    bool inMotion;
+
+    void SetLane(MoveDirection moveDirection, int taps)
+    {        
+        if (moveDirection == MoveDirection.Right)
+        {
+            lane += taps;
+        }
+        else
+        {
+            lane -= taps;
+        }
+
+        tapTimer = 0;
+        inMotion = true;        
+    }
+
+    void Update()
+    {
+        tapTimer += Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0) && tapTimer >= timeBetweenTaps)
+        {
+            if (Input.mousePosition.x > Screen.width / 2)
+            {
+                SetLane(MoveDirection.Right, 1);
+            }
+            else
+            {
+                SetLane(MoveDirection.Left, 1);
+            }
+
+        }
+    }
+
     void FixedUpdate()
     {
-        rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        if (Input.GetKey("d"))
+        rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Force);
+        
+        if (inMotion)
         {
-            rigidBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
+            transform.position = Vector3.Lerp(transform.position, new Vector3(lane * 3f, transform.position.y, transform.position.z), .1f);
 
-        if (Input.GetKey("a"))
-        {
-            rigidBody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            if (rigidBody.transform.position.x == lane * 3f)
+            {
+                inMotion = false;
+            }
         }
+        
 
         if (rigidBody.position.y < -1)
         {
