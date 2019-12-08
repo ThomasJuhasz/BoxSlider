4c55570c21fe5ddf603a0f935ad9c88b086e11bf
ThomasJuhasz <juhasz.thomas.jozsef@gmail.com>
2019-12-08 17:52:06 +0100

    -

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 enum MoveDirection
 {
     Right = 1,
@@ -10,10 +12,23 @@ enum MoveDirection
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rigidBody;
+    public Slider dashCooldown;
     public float speed = 100;
     public float sidewaysForce = 50;
+    public float dashLength = 0;
 
+    private bool isGrounded = true;
     MoveDirection moveDirection = MoveDirection.None;
+
+    void Start()
+    {
+        dashCooldown.value = 1;
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
 
     void Update()
     {
@@ -32,6 +47,28 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDirection = MoveDirection.None;
         }
+
+
+        var horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalMove != 0)
+        {
+            moveDirection = (MoveDirection)horizontalMove;
+        }
+
+        if (Input.GetAxisRaw("Jump") > 0 & isGrounded)
+        {
+            rigidBody.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0 && dashCooldown.value >= 1)
+        {
+            rigidBody.AddForce(new Vector3(0, 0, 30), ForceMode.Impulse);
+            dashCooldown.value = 0;
+        }
+
+        dashCooldown.value += Time.deltaTime;
     }
 
     void FixedUpdate()
